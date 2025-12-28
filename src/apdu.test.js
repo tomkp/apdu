d1a3524d3ada8b98af357c4c8ef0f5a0847ccf88
tomkp <tom@tomkp.com>
2025-12-28 14:31:58 +0000

    Add ESLint and Prettier (#14)
    - Add ESLint with recommended rules and prettier integration
    - Add Prettier with consistent formatting configuration
    - Add lint, lint:fix, format, and format:check scripts
    - Add 'type': 'module' to package.json for native ESM support
    - Format all existing files with Prettier
    
    Fixes #13

--- src/apdu.test.js
+++ src/apdu.test.js
@@ -6,13 +6,13 @@ describe('Apdu', () => {
     it('should construct a basic APDU command (case 1 - no data, no le)', () => {
       const apdu = new Apdu({
         cla: 0x00,
-        ins: 0xA4,
+        ins: 0xa4,
         p1: 0x04,
         p2: 0x00,
       });
 
       expect(apdu.cla).toBe(0x00);
-      expect(apdu.ins).toBe(0xA4);
+      expect(apdu.ins).toBe(0xa4);
       expect(apdu.p1).toBe(0x04);
       expect(apdu.p2).toBe(0x00);
       expect(apdu.size).toBe(4);
@@ -21,7 +21,7 @@ describe('Apdu', () => {
     it('should construct case 2 APDU (no data, with non-zero le)', () => {
       const apdu = new Apdu({
         cla: 0x00,
-        ins: 0xCA,
+        ins: 0xca,
         p1: 0x00,
         p2: 0x00,
         le: 0x10,
@@ -34,13 +34,13 @@ describe('Apdu', () => {
     it('should construct case 3 APDU (with data, no le)', () => {
       const apdu = new Apdu({
         cla: 0x00,
-        ins: 0xA4,
+        ins: 0xa4,
         p1: 0x04,
         p2: 0x00,
-        data: [0xA0, 0x00, 0x00, 0x00, 0x04, 0x10, 0x10],
+        data: [0xa0, 0x00, 0x00, 0x00, 0x04, 0x10, 0x10],
       });
 
-      expect(apdu.data).toEqual([0xA0, 0x00, 0x00, 0x00, 0x04, 0x10, 0x10]);
+      expect(apdu.data).toEqual([0xa0, 0x00, 0x00, 0x00, 0x04, 0x10, 0x10]);
       expect(apdu.lc).toBe(7);
       expect(apdu.size).toBe(16);
     });
@@ -48,14 +48,14 @@ describe('Apdu', () => {
     it('should construct case 4 APDU (with data and le)', () => {
       const apdu = new Apdu({
         cla: 0x00,
-        ins: 0xA4,
+        ins: 0xa4,
         p1: 0x04,
         p2: 0x00,
-        data: [0xA0, 0x00, 0x00, 0x00, 0x04, 0x10, 0x10],
+        data: [0xa0, 0x00, 0x00, 0x00, 0x04, 0x10, 0x10],
         le: 0x00,
       });
 
-      expect(apdu.data).toEqual([0xA0, 0x00, 0x00, 0x00, 0x04, 0x10, 0x10]);
+      expect(apdu.data).toEqual([0xa0, 0x00, 0x00, 0x00, 0x04, 0x10, 0x10]);
       expect(apdu.lc).toBe(7);
       expect(apdu.le).toBe(0x00);
     });
@@ -65,7 +65,7 @@ describe('Apdu', () => {
     it('should return hex string representation', () => {
       const apdu = new Apdu({
         cla: 0x00,
-        ins: 0xA4,
+        ins: 0xa4,
         p1: 0x04,
         p2: 0x00,
       });
@@ -76,10 +76,10 @@ describe('Apdu', () => {
     it('should return hex string with data', () => {
       const apdu = new Apdu({
         cla: 0x00,
-        ins: 0xA4,
+        ins: 0xa4,
         p1: 0x04,
         p2: 0x00,
-        data: [0xA0, 0x00],
+        data: [0xa0, 0x00],
         le: 0x00,
       });
 
@@ -91,25 +91,27 @@ describe('Apdu', () => {
     it('should return byte array', () => {
       const apdu = new Apdu({
         cla: 0x00,
-        ins: 0xA4,
+        ins: 0xa4,
         p1: 0x04,
         p2: 0x00,
       });
 
-      expect(apdu.toByteArray()).toEqual([0x00, 0xA4, 0x04, 0x00, 0x00]);
+      expect(apdu.toByteArray()).toEqual([0x00, 0xa4, 0x04, 0x00, 0x00]);
     });
 
     it('should return byte array with data', () => {
       const apdu = new Apdu({
         cla: 0x00,
-        ins: 0xA4,
+        ins: 0xa4,
         p1: 0x04,
         p2: 0x00,
         data: [0x01, 0x02],
         le: 0x00,
       });
 
-      expect(apdu.toByteArray()).toEqual([0x00, 0xA4, 0x04, 0x00, 0x02, 0x01, 0x02, 0x00]);
+      expect(apdu.toByteArray()).toEqual([
+        0x00, 0xa4, 0x04, 0x00, 0x02, 0x01, 0x02, 0x00,
+      ]);
     });
   });
 
@@ -117,14 +119,14 @@ describe('Apdu', () => {
     it('should return a Buffer', () => {
       const apdu = new Apdu({
         cla: 0x00,
-        ins: 0xA4,
+        ins: 0xa4,
         p1: 0x04,
         p2: 0x00,
       });
 
       const buffer = apdu.toBuffer();
       expect(Buffer.isBuffer(buffer)).toBe(true);
-      expect(buffer).toEqual(Buffer.from([0x00, 0xA4, 0x04, 0x00, 0x00]));
+      expect(buffer).toEqual(Buffer.from([0x00, 0xa4, 0x04, 0x00, 0x00]));
     });
   });
 });
